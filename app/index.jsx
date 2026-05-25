3528f192263cceee6ab42758e06bf2edb95614a2
Kartikey <kartikeypathak08@gmail.com>
2026-05-25 22:18:50 +0530

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className=" text-red-700 text-5xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
